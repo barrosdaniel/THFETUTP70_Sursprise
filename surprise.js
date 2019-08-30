16ce8fcfa62c6aafc050f87270bdaddcc7a0e979
Daniel Barros <barros.daniel@outlook.com>
2019-08-30 15:29:13 +0800

    feature: Add timer to change Surprise section text content

--- surprise.js
+++ surprise.js
@@ -1,5 +1,12 @@
 const surpriseSection = document.getElementById('surprise');
 
-function showSurprise() {
+// function showSurprise() {
+//     surpriseSection.textContent = '🎉 Surprise! 🎉';
+// } Now passed into setTimeout as a callback function
+
+const randomTime = Math.random() * 4000;
+console.log(randomTime);
+
+setTimeout(() => {
     surpriseSection.textContent = '🎉 Surprise! 🎉';
-}
+}, randomTime);
